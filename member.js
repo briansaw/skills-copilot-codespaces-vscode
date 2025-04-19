075d43ab1a927e9c5723eb67b327f819a5b3d1c7
Worrawoot Sawangum <wayb@Worrawoots-Mac-mini.local>
2025-04-19 22:19:07 +0700

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    return {
+        name: 'skills-copilot-codespaces-vscode',
+        description: 'Skills Copilot Codespaces VSCode',
+        type: 'object',
+        properties: {
+        name: {
+            type: 'string',
+            description: 'The name of the skills copilot codespaces vscode.',
+        },
+        description: {
+            type: 'string',
+            description: 'The description of the skills copilot codespaces vscode.',
+        },
+        type: {
+            type: 'string',
+            description: 'The type of the skills copilot codespaces vscode.',
+        },
+        },
+    };
+}
